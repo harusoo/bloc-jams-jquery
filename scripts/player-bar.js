677adff2436c19b8320f28d4a7228d633b3d039c
Heungsoo Kim <hkim450@gmail.com>
2019-01-17 22:36:43 -0600

    Checkpoint sliders completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,7 +16,7 @@
   });
 
   $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') { return; }
+    if (player.playState !== 'playing') {return; }
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
@@ -26,7 +26,16 @@
     player.playPause(previousSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
   setInterval( () => {
     if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
   }, 1000);
 }
